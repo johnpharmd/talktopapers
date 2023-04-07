2e6b39814b088d439795ddab23ae0fff668ff203
johnpharmd <jhumphre@uw.edu>
2023-04-06 18:35:30 -0700

    First release! Basic PubMed search & display

--- streamlit-app/main.py
+++ streamlit-app/main.py
@@ -1,105 +1,34 @@
-from utils import parse_paper, check_folders, download_arxiv
-from openai.embeddings_utils import get_embedding, cosine_similarity
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from Bio import Entrez
 import streamlit as st
 import pandas as pd
-import openai
+import smtplib
 import os
 
+# Set up Entrez access
+email = os.getenv("EMAIL")
+Entrez.email = email
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def search(df, query, n=3, pprint=True):
-    query_embedding = get_embedding(
-        query,
-        engine="text-embedding-ada-002"
-    )
-    df["similarity"] = df.embeddings.apply(lambda x: cosine_similarity(x, query_embedding))
-
-    results = (
-        df.sort_values("similarity", ascending=False)
-        
-    )
-    return results
-
-@st.cache(allow_output_mutation=True)
-def process_file(file):
-    print('[INFO] Processing and calc embeddings')
-    # Process the file and filter
-    with st.spinner(text='Procesing your paper'):
-        paper_text = parse_paper(file)
-        # Apply some filter
-        filtered_paper_text = []
-        for row in paper_text:
-            if len(row['text']) < 30:
-                continue
-            filtered_paper_text.append(row)
-        paper_df = pd.DataFrame(filtered_paper_text)
-        
-    # Calculate embeddings
+def fetch_pubmed_data(keywords):
+    search_handle = Entrez.esearch(db='pubmed', term=keywords, retmax=50)
+    search_results = Entrez.read(search_handle)
+    id_list = search_results['IdList']
+    summary_handle = Entrez.esummary(db='pubmed', id=','.join(id_list))
+    summary_results = Entrez.read(summary_handle)
     
-    with st.spinner(text='Calculate embeddings'):
-        embedding_model = "text-embedding-ada-002"
-        embeddings = paper_df.text.apply([lambda x: get_embedding(x, engine=embedding_model)])
-        paper_df["embeddings"] = embeddings
-    return paper_df
-        
-# Check if data folder exists
-check_folders()
+    return pd.DataFrame(summary_results)
+
 
 if __name__ == '__main__':
-    
-    st.title('Ask the Paper 📚')
-    
-    source = ("PDF", "ARXIV LINK")
-    source_index = st.sidebar.selectbox("Select Input type", range(
-        len(source)), format_func=lambda x: source[x])
-    
-    if source_index == 0:
-        uploaded_file = st.sidebar.file_uploader(
-            "Load File", type=['pdf'])
-        if uploaded_file is not None:
-            # Upload pdf file
-            with st.spinner(text='Uploading your pdf...'):
-                with open(f'data/pdf/{uploaded_file.name}', mode='wb') as w:
-                    w.write(uploaded_file.getvalue())
 
-            paper = process_file(f'data/pdf/{uploaded_file.name}')
-            
-            st.subheader('Now You are ready to ask to your paper')
-            
-            text_input = st.text_input(
-            "Ask your paper here and hit enter 👇",
-            label_visibility='visible',
-            placeholder='Your Question',
-            )
-            if text_input:
-                print(text_input)
-                results = search(paper, text_input, n=3)
-                st.write(results.iloc[0]['text'])
-                
-
-    else:
-        text_input = st.sidebar.text_input(
-            "Enter your arxiv Link here 👇",
-            label_visibility='visible',
-            placeholder='Arvix Link',
-        )
-
-        if text_input:
-            st.sidebar.write("You entered: ", text_input)
-            with st.spinner(text='Procesing your link...'):
-                download_arxiv(text_input)
-                
-            paper = process_file('data/pdf/downloaded-paper.pdf')
-            
-            st.subheader('Now You are ready to ask to your paper')
-            
-            text_input = st.text_input(
-            "Ask your paper here and hit enter 👇",
-            label_visibility='visible',
-            placeholder='Your Question',
-            )
-            if text_input:
-                print(text_input)
-                results = search(paper, text_input, n=3)
-                st.write(results.iloc[0]['text'])
+    # Set up Streamlit app
+    st.title("PubMed Search")
+    query = st.text_input("Enter search terms:")
+    if st.button("Search"):
+        # Retrieve article data from PubMed and display in dataframe
+        articles = fetch_pubmed_data(query)
+        results_df = pd.DataFrame(articles) #, columns=["article"])
+        st.write(results_df)
